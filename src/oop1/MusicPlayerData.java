9cfac047d9a8ce2ddb6e94df5ecaff1d53831f75
apffhel <xxrosevly@gmail.com>
2025-01-16 20:56:46 +0900

    2025.01.16 3.객체 지향 프로그래밍 (절차 지향 프로그래밍)

---
+++ src/oop1/MusicPlayerData.java
@@ -0,0 +1,6 @@
+package oop1;
+
+public class MusicPlayerData {
+    int volume = 0; // 0은 생략해도 자동 초기화됨
+    boolean isOn = false;
+}
---
+++ src/oop1/ValueData.java
@@ -0,0 +1,11 @@
+package oop1;
+
+public class ValueData {
+    int value; //필드
+
+    void add() { //메서드
+        value++;
+        System.out.println("숫자 증가 value=" + value);
+    }
+
+}
