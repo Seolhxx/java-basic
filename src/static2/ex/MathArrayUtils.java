9b0affe1099320499fdad155abbbb6acb95c8cb8
apffhel <xxrosevly@gmail.com>
2025-01-23 12:32:28 +0900

    2025.01.23 7.자바 메모리 구조와 static (문제풀이 전)

---
+++ src/static2/ex/MathArrayUtils.java
@@ -0,0 +1,25 @@
+package static2.ex;
+
+public class MathArrayUtils {
+
+    public static int sum(int[] array) {
+        int sum = 0;
+        for (int i = 0; i <= array.length; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static int average(int[] array) {
+
+        return result;
+    }
+
+    public static int min(int[] array) {
+        return result;
+    }
+
+    public static int max(int[] array) {
+        return result;
+    }
+}
