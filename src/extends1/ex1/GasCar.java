3c31c85beca9af2612d017dea1e4ffc89a542025
apffhel <xxrosevly@gmail.com>
2025-01-24 20:40:27 +0900

    2025.01.24 9.상속

---
+++ src/extends1/ex1/GasCar.java
@@ -0,0 +1,13 @@
+package extends1.ex1;
+
+public class GasCar {
+
+    //휘발유차
+    public void move() {
+        System.out.println("차를 이동합니다.");
+    }
+
+    public void fillUp() {
+        System.out.println("기름을 주유합니다.");
+    }
+}
